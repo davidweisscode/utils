f4083d63e2dc084b8cc446e8759c29547cd0ecb3
David Weiß <davidweisspost@gmail.com>
2022-12-14 23:53:06 +0100

    Rm leading underscore and screenshot string

--- rename_images.sh
+++ rename_images.sh
@@ -10,13 +10,15 @@ do
     new_name=$file
     new_name=$(echo $new_name | sed "s/ //g")
     new_name=$(echo $new_name | sed "s/-/_/g")
-    new_name=$(echo $new_name | sed "s/PXL_//g")
-    new_name=$(echo $new_name | sed "s/IMG_//g")
+    new_name=$(echo $new_name | sed "s/PXL//g")
+    new_name=$(echo $new_name | sed "s/IMG//g")
     new_name=$(echo $new_name | sed "s/WhatsApp//g")
     new_name=$(echo $new_name | sed "s/Image//g")
     new_name=$(echo $new_name | sed "s/Video//g")
     new_name=$(echo $new_name | sed "s/Photo//g")
-    new_name=$(echo $new_name | sed -E 's,([0-9]{2}).([0-9]{2}).([0-9]{2}),20\3\2\1,g')
+    new_name=$(echo $new_name | sed "s/Screenshot//g")
+    new_name=$(echo $new_name | sed "s/^_//")
+    # new_name=$(echo $new_name | sed -E 's,([0-9]{2}).([0-9]{2}).([0-9]{2}),20\3\2\1,g')
     mv -v $file $new_name
 done
 
