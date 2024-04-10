1cc2e4cefdbe528c040b8927cecda4d279c4445a
David Weiß <david.weiss@telekom.de>
2024-04-10 22:14:57 +0200

    Update python image rename script

--- rename_files.py
+++ rename_files.py
@@ -1,4 +1,5 @@
 import os
+import re
 import argparse
 
 
@@ -7,7 +8,7 @@ def get_new_name(filename):
     new_name = filename.lower()
 
     # Replace spaces and underscores with dashes
-    new_name = new_name.replace(" ", "-").replace("_", "-")
+    new_name = new_name.replace(" ", "-").replace("_", "-").replace(".jpeg", ".jpg")
 
     # Remove specific substrings
     patterns_to_remove = [
@@ -30,6 +31,18 @@ def get_new_name(filename):
     # Remove leading and trailing dashes
     new_name = new_name.strip("-")
 
+    file_extension = filename.split(".")[-1]
+
+    # Extract datetime string in the form of yyyymmdd-hhmmss using regex
+    datetime_match = re.search(r"(\d{8}-\d{6})", new_name)
+    if datetime_match:
+        datetime_string = datetime_match.group(1)
+
+        # Drop any characters after the match until the next dash
+        datetime_parts = datetime_string.split("-")[:2]
+        new_name = "-".join(datetime_parts)
+        new_name += f".{file_extension}"
+
     return new_name
 
 
@@ -38,26 +51,32 @@ def rename_files(prod_mode=False):
     print("[START] Rename files\n")
 
     for filename in os.listdir("."):
+        if filename.endswith(".py"):
+            continue
         new_name = get_new_name(filename)
 
         if prod_mode:
             # Rename the file in production mode
+            print(f"{filename.ljust(30)} --> {new_name.ljust(30)} (Rename)")
             os.rename(filename, new_name)
-            print(f"Rename: '{filename}' --> '{new_name}'")
         else:
             # Preview the renaming without actually renaming the files
-            print(f"Preview: '{filename}' --> '{new_name}'")
+            if filename == new_name:
+                print(f"Same name: {new_name}")
+            else:
+                print(f"{filename.ljust(30)} --> {new_name.ljust(30)} (Preview)")
 
     print("\n[END] Rename files\n")
 
 
 if __name__ == "__main__":
-    # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Rename image files")
     parser.add_argument(
-        "--prod", action="store_true", help="Enable production mode (actual renaming)"
+        "--prod",
+        action="store_true",
+        help="Enable production mode (actual renaming)",
+        default=False,
     )
     args = parser.parse_args()
 
-    # Call rename_files function based on argument
     rename_files(prod_mode=args.prod)
